ca0acd20859af2eeeabf81eef4cecb0574f16bf9
sunyiping <52sunyiping@gmail.com>
2024-06-07 08:51:43 +0800

    version 0.1

--- src/components/ProjectPage.vue
+++ src/components/ProjectPage.vue
@@ -12,7 +12,7 @@
             <div class="w3-col l3 m6 w3-margin-bottom" v-for="index in achievement.content" :key="index.id">
                 <div class="w3-display-container">
                     <div class="w3-card-4 w3-margin w3-white">
-                        <img :src="index.Image" style="width:100%">
+                        <img :src="index.Image" style="width:100%" height="175px">
                         <div class="w3-container">
                             <h3><b>{{ index.Title }}</b></h3>
                             <h5>{{ index.Author }}, <span class="w3-opacity">{{ index.Date }}</span></h5>
@@ -44,19 +44,10 @@
     data() {
         return {
             achievements: [
-            { id: 1, content: [
+                { id: 1, content: [
                     {
-                        id: 101,
-                        Image: './ProjectsImg/images.jpeg',
-                        Title: '這是標題',
-                        Author: 'Author',
-                        Date: 'April 7, 2014',
-                        content: '這是內文',
-                        ReadMoreLink: 'https://www.google.com',
-                    },
-                    {
-                        id: 102,
-                        Image: './ProjectsImg/images.jpeg',
+                        id: 104,
+                        Image: './ProjectsImg/image.jpeg',
                         Title: '這是標題',
                         Author: 'Author',
                         Date: 'April 7, 2014',
@@ -65,138 +56,30 @@
                     },
                     {
                         id: 103,
-                        Image: './ProjectsImg/images.jpeg',
-                        Title: '這是標題',
-                        Author: 'Author',
-                        Date: 'April 7, 2014',
-                        content: '這是內文',
-                        ReadMoreLink: 'https://www.google.com',
+                        Image: './ProjectsImg/103Cover.jpeg',
+                        Title: '雙面的早ㄤ',
+                        Author: '孫逸平',
+                        Date: 'May 30, 2024',
+                        content: '不論現在時間，見到人都要說「早安」，這是數位實中的傳統（？',
+                        ReadMoreLink: 'https://www.instructables.com/3D-Dual-Letters/',
                     },
                     {
-                        id: 104,
-                        Image: './ProjectsImg/images.jpeg',
-                        Title: '這是標題',
-                        Author: 'Author',
-                        Date: 'April 7, 2014',
-                        content: '這是內文',
-                        ReadMoreLink: 'https://www.google.com',
-                    },
-                ]
-                },
-                { id: 2, content: [
-                    {
-                        id: 201,
-                        Image: './ProjectsImg/images.jpeg',
-                        Title: '這是標題',
-                        Author: 'Author',
-                        Date: 'April 7, 2014',
-                        content: '這是內文',
-                        ReadMoreLink: 'https://www.google.com',
+                        id: 102,
+                        Image: './ProjectsImg/102Cover.jpeg',
+                        Title: '行星齒輪時鐘 DIY',
+                        Author: '創客社',
+                        Date: 'Mar. 7, 2024',
+                        content: '日本大宮北高校學生來到本校進行國際交流，利用數位實中之諧音「時鐘」，帶領日本的高中生 DIY 製作行星齒輪時鐘。',
+                        ReadMoreLink: 'https://www.instructables.com/Laser-Cut-DIY-Epicyclic-Gearing/',
                     },
                     {
-                        id: 202,
-                        Image: './ProjectsImg/images.jpeg',
-                        Title: '這是標題',
-                        Author: 'Author',
-                        Date: 'April 7, 2014',
-                        content: '這是內文',
-                        ReadMoreLink: 'https://www.google.com',
-                    },
-                    {
-                        id: 203,
-                        Image: './ProjectsImg/images.jpeg',
-                        Title: '這是標題',
-                        Author: 'Author',
-                        Date: 'April 7, 2014',
-                        content: '這是內文',
-                        ReadMoreLink: 'https://www.google.com',
-                    },
-                    {
-                        id: 204,
-                        Image: './ProjectsImg/images.jpeg',
-                        Title: '這是標題',
-                        Author: 'Author',
-                        Date: 'April 7, 2014',
-                        content: '這是內文',
-                        ReadMoreLink: 'https://www.google.com',
-                    },
-                ]
-                },
-                { id: 3, content: [
-                    {
-                        id: 301,
-                        Image: './ProjectsImg/images.jpeg',
-                        Title: '這是標題',
-                        Author: 'Author',
-                        Date: 'April 7, 2014',
-                        content: '這是內文',
-                        ReadMoreLink: 'https://www.google.com',
-                    },
-                    {
-                        id: 302,
-                        Image: './ProjectsImg/images.jpeg',
-                        Title: '這是標題',
-                        Author: 'Author',
-                        Date: 'April 7, 2014',
-                        content: '這是內文',
-                        ReadMoreLink: 'https://www.google.com',
-                    },
-                    {
-                        id: 303,
-                        Image: './ProjectsImg/images.jpeg',
-                        Title: '這是標題',
-                        Author: 'Author',
-                        Date: 'April 7, 2014',
-                        content: '這是內文',
-                        ReadMoreLink: 'https://www.google.com',
-                    },
-                    {
-                        id: 304,
-                        Image: './ProjectsImg/images.jpeg',
-                        Title: '這是標題',
-                        Author: 'Author',
-                        Date: 'April 7, 2014',
-                        content: '這是內文',
-                        ReadMoreLink: 'https://www.google.com',
-                    },
-                ]
-                },
-                { id: 4, content: [
-                    {
-                        id: 401,
-                        Image: './ProjectsImg/images.jpeg',
-                        Title: '這是標題',
-                        Author: 'Author',
-                        Date: 'April 7, 2014',
-                        content: '這是內文',
-                        ReadMoreLink: 'https://www.google.com',
-                    },
-                    {
-                        id: 402,
-                        Image: './ProjectsImg/images.jpeg',
-                        Title: '這是標題',
-                        Author: 'Author',
-                        Date: 'April 7, 2014',
-                        content: '這是內文',
-                        ReadMoreLink: 'https://www.google.com',
-                    },
-                    {
-                        id: 403,
-                        Image: './ProjectsImg/images.jpeg',
-                        Title: '這是標題',
-                        Author: 'Author',
-                        Date: 'April 7, 2014',
-                        content: '這是內文',
-                        ReadMoreLink: 'https://www.google.com',
-                    },
-                    {
-                        id: 404,
-                        Image: './ProjectsImg/images.jpeg',
-                        Title: '這是標題',
-                        Author: 'Author',
-                        Date: 'April 7, 2014',
-                        content: '這是內文',
-                        ReadMoreLink: 'https://www.google.com',
+                        id: 101,
+                        Image: './ProjectsImg/101Cover.jpeg',
+                        Title: '小夜燈工作坊',
+                        Author: '創客社',
+                        Date: 'Nov. 29, 2023',
+                        content: '與北市大同高中 Robot maker 社團進行交流，透過工作坊三個下午的時間，製作獨一無二的小夜燈。',
+                        ReadMoreLink: '',
                     },
                 ]
                 },
